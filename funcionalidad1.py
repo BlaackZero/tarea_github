64778e9480a2d512aee5d5f337b4c38564318add
BlaackZero <blaackzeero@gmail.com>
2023-05-15 10:41:52 -0400

    Cambios de Print Hora

--- funcionalidad1.py
+++ funcionalidad1.py
@@ -5,15 +5,15 @@ import pytz
 # Obtener la hora actual en Chile
 chile_timezone = pytz.timezone('Chile/Continental')
 chile_time = datetime.datetime.now(chile_timezone)
-print("Hora en Chile:", chile_time.strftime("%Y-%m-%d %H:%M:%S"))
+print("Hora en Chile/Continental:", chile_time.strftime("%Y-%m-%d %H:%M:%S"))
 
 # Obtener la hora actual en Japón
 japan_timezone = pytz.timezone('Asia/Tokyo')
 japan_time = datetime.datetime.now(japan_timezone)
-print("Hora en Japón:", japan_time.strftime("%Y-%m-%d %H:%M:%S"))
+print("Hora en Japón/Tokyo:", japan_time.strftime("%Y-%m-%d %H:%M:%S"))
 
 
 # Obtener la hora actual en Canada
 canada_timezone = pytz.timezone('Canada/Pacific')
 canada_time = datetime.datetime.now(canada_timezone)
-print("Hora en Canada:", canada_time.strftime("%Y-%m-%d %H:%M:%S"))
+print("Hora en Canada/Pacific:", canada_time.strftime("%Y-%m-%d %H:%M:%S"))
