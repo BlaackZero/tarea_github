debd2c7ae7ff48c27569daaab0158ecad37b1c27
BlaackZero <blaackzeero@gmail.com>
2023-05-15 10:28:32 -0400

    Create funcionalidad1.py

---
+++ funcionalidad1.py
@@ -0,0 +1,13 @@
+import datetime
+import pytz
+#pip install pytz
+
+# Obtener la hora actual en Chile
+chile_timezone = pytz.timezone('Chile/Continental')
+chile_time = datetime.datetime.now(chile_timezone)
+print("Hora en Chile:", chile_time.strftime("%Y-%m-%d %H:%M:%S"))
+
+# Obtener la hora actual en Japón
+japan_timezone = pytz.timezone('Asia/Tokyo')
+japan_time = datetime.datetime.now(japan_timezone)
+print("Hora en Japón:", japan_time.strftime("%Y-%m-%d %H:%M:%S"))
